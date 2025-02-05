04ee5f33c08ee87815881997b4a02fe4826f3420
PranavKamlaskar <104555599+PranavKamlaskar@users.noreply.github.com>
2025-02-05 17:15:22 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 
-REM ensure docker desktop is running , and ensure you are inside the project directory 
+REM ensure docker desktop is running , and ensure you are inside the project directory , warning only this file is not tested for  windows 
 docker-compose up -d --build
 docker-compose exec web python manage.py migrate
